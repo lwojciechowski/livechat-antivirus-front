2c0ec7a28c883b206b336c0f86b558f87f2efe6c
Łukasz Wojciechowski <nostrzak@gmail.com>
2020-09-10 10:43:24 +0200

    Complete installation flow

--- src/install/Install.js
+++ src/install/Install.js
@@ -1,4 +1,7 @@
+/**@jsx jsx */
+
 import React, {useEffect, useState} from "react";
+import { css, jsx } from "@emotion/core";
 import queryString from "query-string";
 
 import Loading from "../Loading";
@@ -6,7 +9,27 @@ import {createInstallation} from "../api";
 
 const redirectUri = `https://accounts.livechat.com/?response_type=code&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}`;
 
+const containerCss = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2em;
+  strong {
+    font-size: 1.1em;
+  }
+  button {
+    margin-top: 1em;
+  }
+  button:first-of-type {
+    margin-right: 10px;
+  }
+  p {
+    text-align: center;
+  }
+`;
+
 const Install = ({ location }) => {
+    const [loading, setLoading] = useState(true);
     const [queryParams] = useState(queryString.parse(location.search));
 
     useEffect(() => {
@@ -14,6 +37,7 @@ const Install = ({ location }) => {
             if (queryParams.code) {
                 await createInstallation(queryParams.code);
                 window.close();
+                setLoading(false);
                 return;
             }
 
@@ -21,8 +45,17 @@ const Install = ({ location }) => {
         })();
     }, [queryParams.code]);
 
+    if (loading) {
+        return <Loading />
+    }
+
     return (
-        <Loading />
+        <div css={containerCss}>
+            <h2>Installation complete</h2>
+            <p>
+                The plugin will automatically scan all files sent by customers. You may close this window.
+            </p>
+        </div>
     );
 }
 
