0e21761eb0d29f4440087f51d45aed8e34bf84fe
Łukasz Wojciechowski <nostrzak@gmail.com>
2020-09-10 09:55:26 +0200

    Add redirects

--- src/install/Install.js
+++ src/install/Install.js
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from "react";
-import Loading from "../Loading";
 import queryString from "query-string";
+
+import Loading from "../Loading";
 import {createInstallation} from "../api";
 
-
-
 const redirectUri = `https://accounts.livechat.com/?response_type=code&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}`;
+
 const Install = ({ location }) => {
     const [queryParams] = useState(queryString.parse(location.search));
 
